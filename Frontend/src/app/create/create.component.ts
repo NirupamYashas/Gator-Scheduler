3298e8e43f1d5890c099e9c114f608f88eba6143
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2022-04-25 05:28:16 -0400

    added add course form

--- Frontend/src/app/create/create.component.ts
+++ Frontend/src/app/create/create.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup ,Validators } from '@angular/forms';
+import {FormBuilder, FormArray, FormControl, FormGroup ,Validators } from '@angular/forms';
 import { CoursesService } from '../courses/courses.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create',
@@ -10,22 +11,57 @@ import { CoursesService } from '../courses/courses.service';
 })
 export class CreateComponent implements OnInit {
 
-  addCourseForm! : FormGroup; 
+  addCourseForm! : FormGroup;
+  subscription: Subscription;
+  
+  checkboxes = [{
+    name: 'Monday',
+    value: 'monday'
+  }, {
+    name: 'Tuesday',
+    value: 'tuesday'
+  }, {
+    name: 'Wednesday',
+    value: 'wednesday'
+  }, {
+    name: 'Thursday',
+    value: 'thursday'
+  }, {
+    name: 'Friday',
+    value: 'friday'
+  }];
 
-  constructor(public coursesService: CoursesService) { }
+  constructor(public coursesService: CoursesService,private fb: FormBuilder) { }
 
   msgTrue = false;
   ngOnInit(): void {
 
-    this.addCourseForm = new FormGroup({
+    this.addCourseForm = this.fb.group({
       course_name: new FormControl('', [Validators.required]),
       course_code: new FormControl('', [Validators.required]),
       department_name: new FormControl('', [Validators.required]),
       course_instructor: new FormControl('', [Validators.required]),
-      course_days: new FormControl('', [Validators.required]),
-      starting_time: new FormControl('', [Validators.required]),
-      ending_time: new FormControl('', [Validators.required]),
+      course_days: this.fb.array(this.checkboxes.map(x => false),[Validators.required]),
+      // course_days: new FormArray(this.checkboxes.map(x => false), [Validators.required]),
+      monday_starting_time: new FormControl('', [Validators.required]),
+      monday_ending_time: new FormControl('', [Validators.required]),
+      tuesday_starting_time: new FormControl('', [Validators.required]),
+      tuesday_ending_time: new FormControl('', [Validators.required]),
+      wednesday_starting_time: new FormControl('', [Validators.required]),
+      wednesday_ending_time: new FormControl('', [Validators.required]),
+      thursday_starting_time: new FormControl('', [Validators.required]),
+      thursday_ending_time: new FormControl('', [Validators.required]),
+      friday_starting_time: new FormControl('', [Validators.required]),
+      friday_ending_time: new FormControl('', [Validators.required]),
     })
+
+    const checkboxControl = (this.addCourseForm.controls['course_days'] as FormArray);
+    this.subscription = checkboxControl.valueChanges.subscribe(checkbox => {
+      checkboxControl.setValue(
+        checkboxControl.value.map((value:any, i:any) => value ? this.checkboxes[i].value : false),
+        { emitEvent: false }
+      );
+    });
   }
 
   addCourseSubmit(formData: any){
@@ -35,7 +71,11 @@ export class CreateComponent implements OnInit {
     //  var ufMail = this.addProjectForm.getRawValue().uf_mail;
     //  var githubLink = this.addProjectForm.getRawValue().github_link;
     //  const newFormData = { name: projectName, department: departmentName, email: ufMail , link: githubLink };
-    this.coursesService.createCourse(formData).subscribe(data => {})
+    // this.coursesService.createCourse(formData).subscribe(data => {})
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
